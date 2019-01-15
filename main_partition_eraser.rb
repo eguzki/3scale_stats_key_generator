6625077871c76680b8df98218ff390bc1ab2c995
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-01-15 17:14:53 +0100

    test

--- main_partition_eraser.rb
+++ main_partition_eraser.rb
@@ -2,16 +2,17 @@ require 'json'
 require_relative('stats_key_factory')
 require_relative('key_generator')
 
-DELETE_BATCH_SIZE = 50
-
 job = {
-  applications: [1],
-  metrics: Array.new(30) { |idx| (idx + 1) * 10 },
-  service_id: 100,
-  from: Time.new(2017, 1, 1, 1).to_i,
-  to: Time.new(2017, 1, 1, 2).to_i
+  job: {
+    applications: [1],
+    metrics: Array.new(30) { |idx| (idx + 1) * 10 },
+    service_id: 100,
+    from: Time.new(2017, 1, 1, 1).to_i,
+    to: Time.new(2017, 1, 1, 2).to_i
+  },
+  offset: 150,
+  length: 50
 }
-
 
 # TODO Validate method of job
 ## job from and to valid epoch times (in sec)
@@ -20,11 +21,11 @@ job = {
 ## metrics is array
 ## service_id exists, maybe service_id exists in db?
 
-stats_key_types = StatsKeysFactory.create(job)
+stats_key_types = StatsKeysFactory.create(job.job)
 
 stats_key_gen = KeyGenerator.new(stats_key_types)
 
-stats_key_gen.keys.each_slice(DELETE_BATCH_SIZE) do |slice|
+stats_key_gen.keys.drop().take().each do |slice|
   puts '============== delete batch ====================='
   puts slice.inspect
 end
